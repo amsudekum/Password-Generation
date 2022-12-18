5e0814bfb48dc05c7cd5f9791c2c8d321abc03f0
amsudekum <116687202+amsudekum@users.noreply.github.com>
2022-12-18 16:03:37 -0600

    adding prompts

--- script.js
+++ script.js
@@ -10,5 +10,24 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  const length = promptLength();
+  const lowercase = window.confirm('Would you like to include lowercase letters? If no, press cancel.');
+  const uppercase = window.confirm('Would you like to include uppercase letters? If no, press cancel.');
+  const number = window.confirm('Would you like to include numbers? If no, press cancel.');
+  const special = window.confirm('Would you like to include special characters? If no, press cancel.');
+
+}
+
+function promptLength() {
+  const length = window.prompt('How long would you like your password to be?')
+  if (length < 8 || 128 < length) {
+    alert('Please choose a password between 8 and 128 characters.');
+  }
+  return length;
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
